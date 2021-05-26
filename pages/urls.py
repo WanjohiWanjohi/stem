c201a595ab84e4784cca621124f175681e0171ed
Wamuyu Wanjohi <wamuyuwanjohi97@gmail.com>
2021-05-26 13:52:06 +0300

    Making url changes and navigatin active

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,9 @@ from django.urls import  path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+                       path('' , TemplateView.as_view(template_name='home.html') , name='home'),
+                       path('/blog' , TemplateView.as_view(template_name='blog.html') , name='blog'),
+                       path('' , TemplateView.as_view(template_name='home.html')),
+                       path('' , TemplateView.as_view(template_name='home.html')),
                        path('' , TemplateView.as_view(template_name='home.html')),
 ]
