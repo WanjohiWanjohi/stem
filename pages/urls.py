555b6f4ce3712a54e81e64a386b60700f9c77196
Wamuyu Wanjohi <wamuyuwanjohi97@gmail.com>
2021-05-28 10:29:25 +0300

    Adding even more pages@

--- pages/urls.py
+++ pages/urls.py
@@ -4,12 +4,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
                        path('' , TemplateView.as_view(template_name='home.html') , name='home'),
                        path('blog' , TemplateView.as_view(template_name='blog.html') , name='blog'),
-                       #not extended
                        path('join' , TemplateView.as_view(template_name='join.html') , name='join'),
                        path('project' , TemplateView.as_view(template_name='project.html') , name='project'),
                        path('implementation' , TemplateView.as_view(template_name='implementation.html') , name='implementation'),
                        path('labs' , TemplateView.as_view(template_name='labs.html') , name='labs'),
-                       path('equipment' , TemplateView.as_view(template_name='equipment.html') , name='equipment'),
                        path('stem' , TemplateView.as_view(template_name='stem.html') , name='stem'),
                        path('invention' , TemplateView.as_view(template_name='invention.html') , name='invention'),
                        path('coding' , TemplateView.as_view(template_name='coding.html') , name='coding'),
@@ -18,6 +16,8 @@ urlpatterns = [
                        path('competition' , TemplateView.as_view(template_name='competition.html') , name='competition'),
                        path('team' , TemplateView.as_view(template_name='team.html') , name='team'),
                        path('project/robotics' , TemplateView.as_view(template_name='robotics.html') , name='robotics'),
+                       path('project/tinkering' , TemplateView.as_view(template_name='tinkering.html') , name='equipment'),
+                       path('project/equipment' , TemplateView.as_view(template_name='equipment.html') , name='tinkering'),
                     #    path('project/invention' , TemplateView.as_view(template_name='robotics.html') , name='robotics'),
 
 ]
