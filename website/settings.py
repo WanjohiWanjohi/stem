51ef5d1b92d127ddb0761d380e3ec8b2c184844f
Wamuyu Wanjohi <wamuyuwanjohi97@gmail.com>
2021-05-28 11:22:39 +0300

    Removing Keys and debug2

--- website/settings.py
+++ website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6f!evy4bvs+_xh7b^u4+f0+5+e!(v&g^mw$a!)%84q^x85g@2'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
